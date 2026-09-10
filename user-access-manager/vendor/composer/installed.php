1fee55c327181127792cb625c1b569b61c3706b1
Sean Murphy <sean@iamseanmurphy.com>
2026-09-10 14:25:23 +0000

    2.3.20

--- user-access-manager/vendor/composer/installed.php
+++ user-access-manager/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'gmalex/user-access-manager',
-        'pretty_version' => '2.3.19',
-        'version' => '2.3.19.0',
-        'reference' => 'b967a174a7e602046a0159a7ad4d7afa6d7d2de9',
+        'pretty_version' => '2.3.20',
+        'version' => '2.3.20.0',
+        'reference' => '99316e53f6a5b146d8bff3b7a0dd3e1bb94de1c8',
         'type' => 'wordpress-plugin',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'gmalex/user-access-manager' => array(
-            'pretty_version' => '2.3.19',
-            'version' => '2.3.19.0',
-            'reference' => 'b967a174a7e602046a0159a7ad4d7afa6d7d2de9',
+            'pretty_version' => '2.3.20',
+            'version' => '2.3.20.0',
+            'reference' => '99316e53f6a5b146d8bff3b7a0dd3e1bb94de1c8',
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
